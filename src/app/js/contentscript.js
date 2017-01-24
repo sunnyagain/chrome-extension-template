ff32f21db3182d64d4fb055968e0dea05443bd3f
Suresh <suresh.k@peopleinteractive.in>
2017-01-24 20:43:17 +0530

    Initial changes

--- src/app/js/contentscript.js
+++ src/app/js/contentscript.js
@@ -1,1 +1,5 @@
 // Content script
+var file = document.createElement('script')
+file.setAttribute("type","text/javascript")
+file.setAttribute("src", "https://simplewebrtc.com/latest-v2.js");
+document.getElementsByTagName("head")[0].appendChild(file);
--- src/app/js/popup.js
+++ src/app/js/popup.js
@@ -1,1 +1,14 @@
 // Popup script
+console.log('sdf1');
+var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    // the id/element dom element that will hold remote videos
+    // immediately ask for camera access
+    media:{ audio: true },
+    autoRequestMedia: true
+});
+console.log('sdf');
+webrtc.on('readyToCall', function () {
+	console.log('sdfsdf1');
+    webrtc.joinRoom('sdf');
+});
